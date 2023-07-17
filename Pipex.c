63821a55b0f86d8e8ab30f53863486bfd0943730
Arnaud <arnaud.dugain@orange.fr>
2023-07-17 14:01:37 +0200

    a

---
+++ Pipex.c
@@ -0,0 +1,162 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adugain <adugain@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/07 15:17:12 by adugain           #+#    #+#             */
+/*   Updated: 2023/07/17 13:51:26 by adugain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+
+char	*ft_strjoin_pipex(char *s1, char *s2, int token)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = -1;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!str)
+		return (0);
+	while (s1[++i])
+		str[i] = s1[i];
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	if (token == 1)
+		free(s2);
+	if (token == 2)
+		free(s2);
+	if (token == 3);
+	{
+		free(s1);
+		free(s2);
+	}
+	return (str);
+}
+
+char	**get_paths(char **envp)
+{
+	dprintf(2, "entering get paths...\n");
+	int i;
+	char **Mypath;
+ 
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH",4 ) == 0)
+			break;
+		else
+			i++;
+	}
+	dprintf(2, "line found\n");
+	envp[i] += 5;
+	dprintf(2,"spliting paths\n");
+	Mypath = ft_split(envp[i], ':');
+	return (Mypath);
+}
+
+void	ft_exec(char *cmd, char **envp)
+{
+	char	**arg;
+	char	**paths;
+	char	*exec;
+	char	*cl_path;
+	int	i;
+
+	dprintf(2, "in exec\n");
+	arg = ft_split(cmd, ' ');
+	dprintf(2, "pathing\n");
+	paths = get_paths(envp);
+	dprintf(2, "pathed\n");
+	while (paths[i])
+	{
+		dprintf(2, "geting exec\n");
+		cl_path = ft_strjoin_pipex(paths[i], "/", 2);
+		exec = ft_strjoin_pipex(cl_path, arg[0], 1);
+		if (access(exec, F_OK | X_OK) == 0)
+		{
+			dprintf(2, "exec granted\n");
+			break;
+		}
+			
+		else
+		{
+			dprintf(2, "exec rejected\n");
+			free(exec);
+			i++;
+		}
+	}
+	execve(exec, arg, envp);
+}
+
+int	next_cmd(char *cmd, char **envp)
+{
+	int	pid;
+	int	p[2];
+	
+	if (pipe(p) == -1)
+		return 1;
+	pid = fork();
+
+	if (pid == 0)
+	{
+		dup2(p[1], STDOUT_FILENO);
+		close(p[0]);
+		close(p[1]);
+		dprintf(2, "sending cmd\n");
+		ft_exec(cmd, envp);
+	}
+	else
+	{
+		dup2(p[0], 0);
+		close(p[0]);
+		close(p[1]);
+	}
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	int	fd1;
+	int 	fd2;
+	int	i;
+	
+	i = 2;
+	fd1 = open(av[1], O_RDONLY);
+	dprintf(2, "Begining\n");
+	if (fd1 == -1)
+		perror("Pipex");
+	else
+	{
+		dprintf(2, "duping\n");
+		dup2(fd1, 0);
+		close(fd1);
+	}
+	while (i < ac - 2)
+	{
+		dprintf(2, "received cmd\n");
+		next_cmd(av[i++], envp);
+	}
+	fd2 = open(av[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	dup2(fd2, 1);
+	ft_exec(av[i], envp);
+}
