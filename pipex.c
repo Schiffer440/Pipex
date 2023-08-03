f004560ef65491646cf6487afbb03a0f95b72371
Arnaud <arnaud.dugain@orange.fr>
2023-08-04 00:22:21 +0200

    all running good not normed

--- pipex.c
+++ pipex.c
@@ -6,66 +6,70 @@
 /*   By: adugain <adugain@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 15:17:12 by adugain           #+#    #+#             */
-/*   Updated: 2023/08/03 14:01:38 by adugain          ###   ########.fr       */
+/*   Updated: 2023/08/04 00:21:04 by adugain          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	next_cmd(char *cmd, char **envp)
+static void	next_cmd(char *cmd, char **envp, char *std)
 {
 	pid_t	pid;
-	// pid_t	save_pid;
 	int	p[2];
 
 	if (pipe(p) == -1)
 		ft_perror("Failed to pipe", 1);
 	pid = fork();
-	// if (pid == 0)
-	// 	save_pid = pid;
-	// else
-	// 	save_pid = 1;
 	if (pid == 0)
 	{
 		close(p[0]);
 		dup2(p[1], 1);
-		// close(p[1]);
-		// dup_pipeout(&(*p));
 		ft_exec(cmd, envp);
-		// close(p[0]);
 		close(p[1]);
 	}
 	else
 	{	
 		
 		close(p[1]);
-		// dup2(p[0], 0);
-		// if (save_pid != 1)
-		// 	waitpid(save_pid, &save_pid, 0);
+		if (ft_strncmp(std, "/dev/stdin", sizeof(std) - 1) != 0)
+			dup2(p[0], 0);
 		close(p[0]);
-		// wait(NULL);
-		// ft_exec(cmd, envp);
-		// dup_pipein(&(*p));
 	}
-	// ft_exec(cmd, envp);
 }
 
-// static void	last_cmd(char *cmd, char **envp)
-// {
-// 	int	pid;
-
-// 	pid = fork();
-// 	if (pid == 0)
-// 		ft_exec(cmd, envp);	
-// }
+static	void	last_cmd(char **av, char **envp, int *i)
+{
+	pid_t	pid;
+	int	p[2];
+	int	fd2;
+	
+	if (pipe(p) == -1)
+		ft_perror("Failed to pipe", 1);
+	pid = fork();
+	if (pid == 0)
+	{
+		close(p[0]);
+		fd2 = open(av[*i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fd2 == -1)
+			ft_perror(ft_strjoin(("Error opening "), av[*i + 1]), 1);
+		dup2(fd2, 1);
+		close(fd2);
+		close(p[1]);
+		ft_exec(av[*i], envp);
+	}
+	else
+	{
+		close(p[1]);
+		dup2(p[0], 0);
+		close(p[0]);
+	}
+}
 
 void	pipex(int ac, char **av, char **envp)
 {
 	int	fd1;
-	int	fd2;
 	int	i;
-	pid_t	pid;
-
+	
 	i = 2;
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
@@ -73,34 +77,11 @@ void	pipex(int ac, char **av, char **envp)
 	else
 	{
 		dup2(fd1, 0);
-		// close(fd1);
+		close(fd1);
 	}
-	// fd2 = open(av[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	// if (fd2 == -1)
-	// 	ft_perror(ft_strjoin(("Error opening "), av[i + 1]), 1);
-	// dup2(fd2, STDOUT_FILENO);
 	while (i < ac - 2)
-	{
-		next_cmd(av[i++], envp);
-		// ft_exec(av[i], envp);
-	}
-	// last_cmd(av[i], envp);
-	// ft_exec(av[i], envp);
-	pid = fork();
-	if (pid == 0)
-	{
-		fd2 = open(av[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (fd2 == -1)
-			ft_perror(ft_strjoin(("Error opening "), av[i + 1]), 1);
-		dup2(fd2, 1);
-		// close(fd2);
-		ft_exec(av[i], envp);
-		close(fd2);
-		// exit(1);
-	}
-		// wait(NULL);
-	close(fd1);
-	// close(fd2);
+		next_cmd(av[i++], envp, av[1]);
+	last_cmd(av, envp, &i);
 }
 
 static void	get_temp(int fd, char *end_of_file)
@@ -126,10 +107,37 @@ static void	get_temp(int fd, char *end_of_file)
 	free(line);
 }
 
+static	void	last_cmd_heredoc(char **av, char **envp, int *i)
+{
+	pid_t		pid;
+	int	p[2];
+	int	fd1;
+
+	if (pipe(p) == -1)
+		ft_perror("Failed to pipe", 1);
+	pid = fork();
+	if (pid == 0)
+	{
+		close(p[0]);
+		fd1 = open(av[*i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (fd1 == -1)
+			ft_perror(ft_strjoin(("Error opening "), av[*i + 1]), 1);
+		dup2(fd1, 1);
+		close(fd1);
+		close(p[1]);
+		ft_exec(av[*i], envp);
+	}
+	else
+	{	
+		close(p[1]);
+		dup2(p[0], 0);
+		close(p[0]);
+	}
+}
+
 void	pipex_here_doc(int ac, char **av, char *end_of_file, char **envp)
 {
 	int		fd;
-	int		fd1;
 	char	*temp;
 	int		i;
 
@@ -144,14 +152,9 @@ void	pipex_here_doc(int ac, char **av, char *end_of_file, char **envp)
 	dup2(fd, STDIN_FILENO);
 	close(fd);
 	while (i < ac - 2)
-		next_cmd(av[i++], envp);
-	fd1 = open(av[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd1 == -1)
-		ft_perror(ft_strjoin("Error opening ", av[i + 1]), 127);
-	dup2(fd1, STDOUT_FILENO);
-	close(fd1);
+		next_cmd(av[i++], envp, av[1]);
+	last_cmd_heredoc(av, envp, &i);
 	unlink(temp);
-	ft_exec(av[i], envp);
 }
 
 int	main(int ac, char **av, char **envp)
@@ -171,6 +174,9 @@ int	main(int ac, char **av, char **envp)
 		return (0);
 	}
 	else
-		ft_printf("usage: ./pipex file1 cmd1 cmd2 file2");
+	{
+		ft_printf("usage: ./pipex infile cmd1 cmd2 ... outfile");
+		ft_printf(" heredoc usage: ./pipex here_doc cmd1 cmd2 ... outfile");
+	}
 	return (0);
 }
